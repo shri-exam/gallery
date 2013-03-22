ec561d03fe12ad654fdadda23f7ca1295e2b05e5
vvv <ee923925@gmail.com>
2013-03-22 07:50:51 +0200

    add function makeGet

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 (function($){
     $(function(){
         /* ########################## */
-        function l(){console.log(l);}
+        var user = 'aig1001',albom = '63684'; // Имя пользователя и название альбома на яндекс фотках
+        var ajaxResult;
+        function l(l){console.log(l);}
 
+        function makeGet(url){
+            $.ajax({
+                contentType: 'application/json',
+                url: 'http://api-fotki.yandex.ru'+url+'?format=json&callback=?',
+                dataType: 'jsonp',
+                success: function(data){ l(data);}
+            });
+        }
         /* ########################## */
     });
 })(jQuery);
